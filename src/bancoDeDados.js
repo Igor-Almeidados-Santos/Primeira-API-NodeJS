cc924296c3304dd2d8a59348c129da7a131bc5e1
IGOR ALMEIDA DOS SANTOS <75753575+Igor-Almeidados-Santos@users.noreply.github.com>
2021-04-17 10:35:57 -0300

    Primeira API
    API NodeJS

---
+++ src/bancoDeDados.js
@@ -0,0 +1,23 @@
+const sequence ={
+    __id: 1,
+    get id(){ return this.__id++ }
+};
+
+const produtos = {};
+
+//Esta função salva o produto do banco de dados no objeto produto.
+function salvarProduto(produto) {
+    if(!produto.id) produto.id = sequence.id;//Verifica se id de produto está setado, se não ele adiciona o id retornado do metodo no objeto sequence.
+    produtos[produto.id] = produto;//Aqui adiciona o produto na lista/objeto produtos.
+    return produto;
+};
+
+function getProduto(id) {//Retorna o produto referente ao id informado se não encontrar retorna um objeto vazio.
+   return produtos[id] || {}; 
+};
+
+function getProdutos() {//Retorna todos os produtos dentro do objeto produtos.
+    return Object.values(produtos);
+};
+
+module.exports = {salvarProduto, getProduto, getProdutos};//Deixa estas funções viziveis para usar em outros módulos.
--- src/servidor.js
+++ src/servidor.js
@@ -1,10 +1,27 @@
 const porta = 3003;//Uma porta é um processo dentro do computador.
 
+const { response } = require( 'express' );
 const express = require('express');
 const app = express();
+const bancoDeDados = require('./bancoDeDados');
 
 app.get('/produtos', (req, res, next) => {
-    res.send({nome: 'Notebook', preco: 1234.56});//Envia uma resposta que é convertida para o formato JSON.
+    res.send(bancoDeDados.getProdutos());//Retorna a lista de produtos.
+    
+    // res.send({nome: 'Notebook', preco: 1234.56});//Envia uma resposta que é convertida para o formato JSON.
+});
+
+app.get('/produtos/:id', (req, res, next)=> {//Retorna um produto referente a id informada.
+    res.send(bancoDeDados.getProduto(req.params.id));
+});
+
+app.post('/produtos', (req, res, next) => {//Salva o produto na memória.
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    });
+
+    response.send(produto);//Converte o objeto produto em um JSON para ser enviado para a web.
 });
 
 app.listen(porta, () => {
